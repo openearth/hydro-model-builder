e8dc34b5f9066594b79a07fe2c2988f4945db76a
gena <gennadiy.donchyts@gmail.com>
2018-06-21 15:38:07 +0200

    add options

--- hydro_model_builder/cli.py
+++ hydro_model_builder/cli.py
@@ -3,16 +3,35 @@
 """Console script for hydro_model_builder."""
 import sys
 import click
+import logging
 
+logger = logging.getLogger(__name__)
 
-@click.command()
-def main(args=None):
-    """Console script for hydro_model_builder."""
-    click.echo("Replace this message by putting your code into "
-               "hydro_model_builder.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+@click.group()
+def cli():
+    pass
+
+@click.command(name='generate-model')
+@click.option('-t', '--template', required=True)
+@click.option('-o', '--options-file', required=True)
+@click.option('-r', '--results-dir', required=True)
+def generate_model(template, options_file, results_dir):
+    print(template)
+    print(options_file)
+    print(results_dir)
+    click.echo('Generate model for a given region')
+
+# @click.command(name='upload-data')
+# @click.option('-o', '--options', required=True)
+# def upload():
+#     click.echo('Upload model or data')
+
+cli.add_command(generate_model)
+cli.add_command(upload)
+
+def main():
+    cli()
     return 0
-
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,12 +2,27 @@ from click.testing import CliRunner
 
 from hydro_model_builder import cli
 
-def test_command_line_interface():
+def test_cli_main():
     """Test the CLI."""
     runner = CliRunner()
+
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
     assert 'hydro_model_builder.cli.main' in result.output
+
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
+
+def test_cli_generate_model_help():
+    """Show help for generate-model."""
+    runner = CliRunner()
+
+    result = runner.invoke(cli.main)
+    assert result.exit_code == 0
+    assert 'hydro_model_builder.cli.main' in result.output
+
+    help_result = runner.invoke(cli.main, ['--help'])
+    assert help_result.exit_code == 0
+    assert '--help  Show this message and exit.' in help_result.output
+
