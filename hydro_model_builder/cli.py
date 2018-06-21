a4532f431c9f1b282e5b1f859a21c68a956872de
gena <gennadiy.donchyts@gmail.com>
2018-06-21 15:38:41 +0200

    add options

--- hydro_model_builder/cli.py
+++ hydro_model_builder/cli.py
@@ -21,13 +21,14 @@ def generate_model(template, options_file, results_dir):
     print(results_dir)
     click.echo('Generate model for a given region')
 
+cli.add_command(generate_model)
+
 # @click.command(name='upload-data')
 # @click.option('-o', '--options', required=True)
 # def upload():
 #     click.echo('Upload model or data')
+# cli.add_command(upload)
 
-cli.add_command(generate_model)
-cli.add_command(upload)
 
 def main():
     cli()
