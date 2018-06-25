df1166dad1b57ad94800080d14a48c8cd371f2f0
gena <gennadiy.donchyts@gmail.com>
2018-06-25 13:49:23 +0200

    * fix docs * move YAML to model_builder

--- hydro_model_builder/model_builder.py
+++ hydro_model_builder/model_builder.py
@@ -3,3 +3,16 @@
 """Main module."""
 
 # import model_generators as mg
+
+import yaml
+import geojson
+
+class ModelBuilder(object):
+    def __init__(self, configfile):
+        self.config = None
+        self.configfile = configfile
+
+    def parse_config(self):
+        with open(self.configfile) as f:
+            d = yaml.safe_load(f)
+        self.config = d
