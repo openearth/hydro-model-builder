e5e8508ef2d92e829ca254f5c703393bd5dd159c
Martijn Visser <mgvisser@gmail.com>
2018-07-05 16:57:42 +0200

    download rasters in cli
    and flesh out the yaml

--- hydro_model_builder/cli.py
+++ hydro_model_builder/cli.py
@@ -4,7 +4,9 @@
 import sys
 import click
 import logging
+from pathlib import Path
 
+import hydroengine
 from hydro_model_builder.model_builder import ModelBuilder
 
 logger = logging.getLogger(__name__)
@@ -28,9 +30,40 @@ def generate_model(options_file, results_dir):
     dicts = builder.parse_config(options_file)
     genopt, modopt = dicts
     # TODO validate config
+    # TODO fill in all defaults (for now we should supply all)
     msg = f"Going to create a '{genopt['model']}'/'{modopt['concept']}' model, it will be placed in '{results_dir}'"
+    general_options(genopt)
     click.echo(msg)
     click.echo(builder.get_generator_names())
+
+
+def get_hydro_data(region, ds):
+    if ds["type"] == "raster" and ds["source"] == "earth-engine":
+        # create directory
+        Path(ds["path"]).parent.mkdir(parents=True, exist_ok=True)
+        print("Downloading", ds["variable"])
+        hydroengine.download_raster(
+            region,
+            ds["path"],
+            ds["variable"],
+            ds["cell_size"],
+            ds["crs"],
+            ds["region_filter"],
+            ds["catchment_level"],
+        )
+
+
+def general_options(d):
+    # get data from hydro-engine one by one
+    defaults = d["hydro-engine"]["defaults"]
+    for ds_override in d["hydro-engine"]["datasets"]:
+        ds = defaults.copy()
+        ds.update(ds_override)
+        if ds["source"] == "earth-engine":
+            get_hydro_data(d["region"], ds)
+        else:
+            # TODO support non earth-engine datasets
+            print("skipped variable:", ds["variable"])
 
 
 main.add_command(generate_model)
