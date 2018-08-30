21ea7215d2a9afd6ee9a75e68c109a278bc63beb
Martijn Visser <mgvisser@gmail.com>
2018-08-30 16:43:00 +0200

    hook up wflow

--- hydro_model_builder/cli.py
+++ hydro_model_builder/cli.py
@@ -5,8 +5,9 @@ import sys
 import click
 import logging
 from pathlib import Path
-
+from shapely import geometry as sg
 import hydroengine
+import numpy as np
 from hydro_model_builder.model_builder import ModelBuilder
 from hydro_model_generator_wflow import ModelGeneratorWflow
 
@@ -42,11 +43,29 @@ def generate_model(options_file, results_dir, skip_download):
 
 
 
+def utm_epsg(region):
+    """Return UTM EPSG code for a given region geojson feature"""
+    centroid = sg.shape(region).centroid
+    longitude, latitude = centroid.x, centroid.y
+    
+    # northern latitudes
+    if latitude > 0:
+        UTMzone = int((np.floor((longitude + 180.0) / 6.0) % 60.0) + 1)
+        epsg = 32600 + UTMzone
+    # southern latitudes
+    else:
+        UTMzone = int((np.floor((longitude + 180.0) / 6.0) % 60.0) + 1)
+        epsg = 32700 + UTMzone
+        
+    return epsg
+
+
 def get_hydro_data(region, ds):
-    if ds["type"] == "raster" and ds["source"] == "earth-engine":
+    if ds["function"] == "get-raster" and ds["source"] == "earth-engine":
         # create directory
         Path(ds["path"]).parent.mkdir(parents=True, exist_ok=True)
-        print("Downloading", ds["variable"])
+        if ds["crs"].lower() == "utm":
+            ds["crs"] = f"EPSG:{utm_epsg(region)}"
         hydroengine.download_raster(
             region,
             ds["path"],
@@ -56,6 +75,12 @@ def get_hydro_data(region, ds):
             ds["region_filter"],
             ds["catchment_level"],
         )
+    elif ds["function"] == "get-catchments" and ds["source"] == "earth-engine":
+        hydroengine.download_catchments(region, ds["path"], ds["region_filter"], ds["catchment_level"])
+    elif ds["function"] == "get-rivers" and ds["source"] == "earth-engine":
+        filter_upstream_gt = 1000
+        hydroengine.download_rivers(region, ds["path"], filter_upstream_gt, ds["region_filter"], ds["catchment_level"])
+
 
 
 def general_options(d):
